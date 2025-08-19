883cea5dccc885f62e6640040240192d517c951e
Gabriel Kaue <gabrielkauerodriguest@gmail.com>
2025-08-18 21:24:51 -0300

    feat: atividade1

--- JavaBasicsDanielSouza/Avaliacao2/src/Atividade1.java
+++ JavaBasicsDanielSouza/Avaliacao2/src/Atividade1.java
@@ -4,6 +4,21 @@ public class Atividade1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("2\t5");
+        System.out.print("Digite um número inteiro, um decimal e um nome separado por (tab): ");
+        String linha = sc.nextLine();
+
+        //METODO SLIPT
+
+        String[] partes =linha.split("\t");
+
+        int inteiro = Integer.parseInt(partes[0]);
+        double decimal = Double.parseDouble(partes[1]);
+        String nome = partes[2];
+
+        System.out.println("Você digitou o inteiro: " + inteiro);
+        System.out.println("Você digitou o ponto flutuante: " + decimal);
+        System.out.println("Você digitou o nome composto: " + nome);
+
+        sc.close();
     }
 }
